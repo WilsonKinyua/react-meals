0c0ee95199e79171c356225a10729531f2376e19
WilsonKinyua <wilsonkinyuam@gmail.com>
2022-04-09 12:26:52 +0300

    setting up input fields

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,5 +1,23 @@
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
+
 const MealItemForm = () => {
-    return;
-}
+  return (
+    <form className={classes.form}>
+      <Input
+        input={{
+          type: "number",
+          id: "amount",
+          min: 1,
+          max: 5,
+          step: "1",
+          defaultValue: "1",
+        }}
+        label="Amount"
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
 
 export default MealItemForm;
