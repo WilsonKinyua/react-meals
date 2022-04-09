60481eb33c8a68e9aa04a7bc1aeb98024b86fc5f
WilsonKinyua <wilsonkinyuam@gmail.com>
2022-04-09 12:34:59 +0300

    making the input unique

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,13 +1,13 @@
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
-const MealItemForm = () => {
+const MealItemForm = (props) => {
   return (
     <form className={classes.form}>
       <Input
         input={{
           type: "number",
-          id: "amount",
+          id: "amount_" + props.id,
           min: 1,
           max: 5,
           step: "1",
